4b8cbb6c1095defb570915ff9093b80b78727fb5
fineday1 <temp15614@gmail.com>
2025-05-22 12:14:11 +0200

    Create 107binaryTreeLevelOrderTraversal2.cpp

---
+++ 107binaryTreeLevelOrderTraversal2.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrderBottom(TreeNode* root) {
+        vector<vector<int>> result;
+        if (!root) return result;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int levelSize = q.size();
+            vector<int> currentLevel;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode *node = q.front();
+                q.pop();
+                if (node->left) q.push(node->left);
+                if (node->right) q.push(node->right);
+                currentLevel.push_back(node->val);
+            }
+            result.push_back(currentLevel);
+            // we can either reverse the entire 2d vector at the end or insert each level at the beginning
+            // of the 2d vector
+            // result.insert(result.begin(), currentLevel);
+        }
+        std::reverse(result.begin(), result.end());
+        return result;
+    }
+};
